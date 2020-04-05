43c8468680e1a009f0dc970fdb98f5ebb392b622
Kevin Mathias <komathias1@gmail.com>
2020-04-05 22:43:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram_words)
+    array_new = []
+    words.each{|word| word.split("").sort ==@word.split("").sort}
+    array_new << word
+    array_new
+  end
+  
+  anagram_words.select 
